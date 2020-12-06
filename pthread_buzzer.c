334426dccfdfe55481e069da0dcc1e45dcf8db67
Lcy <beonerun@163.com>
2020-12-06 23:11:52 +0800

    A53 program source code

---
+++ pthread_buzzer.c
@@ -0,0 +1,37 @@
+#include "data_global.h"
+
+extern int dev_buzzer_fd;
+extern char dev_buzzer_mask;
+
+extern pthread_mutex_t mutex_buzzer;
+
+extern pthread_cond_t cond_buzzer;
+
+void *pthread_buzzer (void *arg)
+{
+	struct input_event event;
+	if ((dev_buzzer_fd = open (DEV_BUZZER, O_RDWR | O_NONBLOCK)) < 0)
+	{
+		printf ("Cann't open file /dev/beep\n");
+		exit (-1);
+	}
+	printf ("pthread_buzzer is ok\n");
+
+	while (1)
+	{
+		pthread_mutex_lock (&mutex_buzzer);
+		pthread_cond_wait (&cond_buzzer, &mutex_buzzer);
+		printf ("pthread_buzzer is wake up\n");
+		if (dev_buzzer_mask){
+			write(dev_buzzer_fd, &event, sizeof(struct input_event));
+			printf("**buzzer on\n");
+		}else{
+			//ioctl(dev_buzzer_fd, BEEP_OFF);
+			write(dev_buzzer_fd, &event, sizeof(struct input_event));
+			printf("**buzzer off\n");
+		}
+		pthread_mutex_unlock (&mutex_buzzer);
+	}
+
+	return 0;
+}
